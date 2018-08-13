e1703b41f2cb702e332d17a3538a31c6b59a168a
Viswa Bharath <viswa.bharath@docsapp.in>
2018-08-13 23:53:36 +0530

    Initial Commit

---
+++ Moovup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moovup
+//
+//  Created by Vishwa Bharath on 13/08/18.
+//  Copyright © 2018 ViswaBharathD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
